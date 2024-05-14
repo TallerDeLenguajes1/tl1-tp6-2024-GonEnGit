830ecd1262397dca6cee7f310aae660d546617ab
Gonzalo Javier Martinez <goncham94@gmail.com>
2024-05-14 15:46:37 -0300

    primer commit, punto 1 terminado

---
+++ tp6_1/tp6_1.cs
@@ -0,0 +1,31 @@
+﻿
+int numero;
+int invertido = 0;
+int auxiliar;
+string ingreso;
+bool respuesta;
+
+
+do
+{
+    Console.WriteLine("Ingrese un numero: ");
+    ingreso = Console.ReadLine();
+
+    // esta funcion ya te guarda lo que esté en ingreso en numero
+    // no necesitas volver a igualar mas abajo
+    respuesta = int.TryParse(ingreso, out numero);
+    if (respuesta == false)
+    {
+        Console.WriteLine("Este programa solo acepta numeros.");
+    }
+    
+} while (respuesta == false);
+
+while (numero > 0)
+{
+    auxiliar = numero % 10;
+    invertido = (invertido * 10) + auxiliar;
+    numero = numero / 10;
+}
+
+Console.WriteLine("El numero invertido es: " + invertido);
