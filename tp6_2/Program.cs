e1ca399b576cbcfea10de120c5e490aed93ef2d7
Gonzalo Martinez <mgonzalo192@gmail.com>
2024-05-20 20:57:21 -0300

    tercer commit, segunda calculado subida a rama calculadoraV2 antes de merge

--- tp6_2/Program.cs
+++ tp6_2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int primerNum;
+int segundoNum;
+int tercerNum;
+int absoluto;
+double cuadrado;
+double raiz;
+double seno;
+double coseno;
+int parteEntera;
+string ingreso;
+bool prueba;
+
+do
+{
+    Console.WriteLine("Ingrese un numero: ");
+    ingreso = Console.ReadLine();
+
+    prueba = int.TryParse(ingreso, out primerNum);
+    if (!prueba)
+    {
+        Console.WriteLine("Debe ingresar un numero.");
+    }
+} while (!prueba);
+
+// busca si existen funciones para esto
+absoluto = Math.Abs(primerNum);
+cuadrado = Math.Pow(primerNum, 2);
+raiz = Math.Sqrt(primerNum);
+seno = Math.Sin(primerNum);
+coseno = Math.Cos(primerNum);
+
+Console.WriteLine("Valor Absoluto = " + absoluto);
+Console.WriteLine("Cuadrado = " + cuadrado);
+Console.WriteLine("Raiz = " + raiz);
+Console.WriteLine("Seno = " + seno);
+Console.WriteLine("Coseno = " + coseno);
